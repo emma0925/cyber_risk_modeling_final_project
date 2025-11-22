cdf4d6e20b8c701b986a8e66e6ef038e37637887
emma0925 <jianyun.zhuang@mail.utoronto.ca>
2025-11-22 15:45:02 -0500

    Add GitHub Pages deployment workflow

--- cmmc-incident-site/vite.config.js
+++ cmmc-incident-site/vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  plugins: [react()],
+  base: '/cyber_risk_modeling_final_project/',
 })
